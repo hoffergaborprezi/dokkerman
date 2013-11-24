72854ead3abb72fbdab39068612690309a568d62
Gabor Hoffer <gabor.hoffer@prezi.com>
2013-11-24 16:09:03 +0100

    Moving the facebook parser to separate file

---
+++ assets/js/dokkerman.js
@@ -0,0 +1,71 @@
+/*
+            .---._------------------------------+
+___________/ ._____)                            |
+              ) __|          DOKKERMAN          |
+                __|   and the turkeying fellaz  |
+..---------.._____|                             |
+                  +-----------------------------+
+ */
+
+ window.fbAsyncInit = function() {
+	    // init the FB JS SDK
+	    FB.init({
+	      appId      : '587719267937359',                       // App ID from the app dashboard
+	      channelUrl : '', 										// Channel file for x-domain comms
+	      status     : true,                                 	// Check Facebook Login status
+	      cookie	 : true,
+	      xfbml      : true                                  	// Look for social plugins on the page
+	    });
+	    var token;
+	    // TODO: proper authentication
+		FB.getLoginStatus(function (response) {
+			$.getJSON("https://graph.facebook.com/dokkerman/posts?access_token=587719267937359|3faac613025bbfaa2346f45c681ac7ba&callback=?",function(json){
+				for (item in json.data) {
+					var appendOutput = '';
+				    appendOutput += '<div class="item">';
+				    if (json.data[item].picture) { appendOutput += '<a href="https://www.facebook.com/' + json.data[item].id + '/" target="_blank"><img src="' + json.data[item].picture + '" width="100%" /></a>'; }
+				    if (json.data[item].name) { appendOutput += '<h3>' + json.data[item].name + '</h3>'; }
+				    if (json.data[item].story) { appendOutput += '<h3>' + json.data[item].story + '</h3>'; }
+				    if (json.data[item].message) { appendOutput += '<p>' + json.data[item].message +'</p>'; }
+				    appendOutput += '</div>';
+				    $("#postsFromFacebook").append(appendOutput);
+				    
+				    //getItemDescription(json.data[item].id);
+				}
+				// Wookmark init script
+				$('.item').wookmark({
+				  align: 'center',
+				  autoResize: true,
+				  container: $('#postsFromFacebook'),
+				  direction: 'left',
+				  ignoreInactiveItems: true,
+				  itemWidth: 200,
+				  fillEmptySpace: false,
+				  flexibleWidth: false,
+				  offset: 10,
+				  onLayoutChanged: undefined,
+				  outerOffset: 0,
+				  resizeDelay: 50,
+				  possibleFilters: []
+				});
+			});
+
+			// Get and parse event descriptions. This is a separate call for each event.
+			function getItemDescription(id) {
+			    $.getJSON("https://graph.facebook.com/"+id+"?access_token="+token+"&callback=?",function(jsonItem) {
+			     	$("#event"+id).html('<p>' +jsonItem.description+'</p>');
+			    });
+			}
+
+		});
+	    // Additional initialization code such as adding Event Listeners goes here
+	  };
+
+	  // Load the SDK asynchronously
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/all.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
